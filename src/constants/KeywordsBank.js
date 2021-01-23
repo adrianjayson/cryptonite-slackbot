14728f9fed6fba5430a16f83ec8d91ad645a9ea4
AJ Catambay <adrianjayson13@gmail.com>
2021-01-23 19:55:16 +0800

    Creates constants file for Keywords and responses.

---
+++ src/constants/KeywordsBank.js
@@ -0,0 +1,8 @@
+module.exports = {
+    HELLO: ['hi', 'hello', 'hey', 'what\'s up', 'sup'],
+    COIN_PRICE_ANY_KW: ['how much'],
+    COIN_PRICE_ALL_KW: ['what', 'price'],
+    COIN_UPDATE_ANY_KW: ['give me an update', 'update me', 'how\'s', 'what about'],
+    COIN_UPDATE_ALL_KW: ['how', 'doing'],
+    COINS_UPDATE_ANY_KW: ['all cryptocurrencies', 'all cryptos', 'all coins', 'brief me']
+};
---
+++ src/constants/Responses.js
@@ -0,0 +1,5 @@
+module.exports = {
+    GREET: 'Hi there! What can I do for you?',
+    CRYPTO_UNAVAILABLE: 'Apologies. I\'m unable to retrieve the data for that cryptocurrency for now. Can you try again later?',
+    CRYPTO_NOT_FOUND: 'Apologies. I believe I do not have the data for that cryptocurrency.'
+}
